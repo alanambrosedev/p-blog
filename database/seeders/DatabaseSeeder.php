32dc3ec593a05b7acef3391d6e1f589bbdaba94a
Alan Ambrose <alanambrosedev@gmail.com>
2026-04-06 22:05:59 +0530

    feat: initialize routes and testing structure
    - Added API routes for user authentication in `routes/api.php`.
    - Created console command for inspiring quotes in `routes/console.php`.
    - Set up a basic web route in `routes/web.php`.
    - Configured `.gitignore` files for various storage directories to manage ignored files.
    - Implemented initial test cases in `tests/Feature/ExampleTest.php` and `tests/Unit/ExampleTest.php`.
    - Established a base test case class in `tests/TestCase.php`.
    - Introduced Pest testing framework configuration in `tests/Pest.php`.
    - Configured Vite for asset management in `vite.config.js`.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'admin@2026',
+        ]);
+
+        $categories = collect(['Laravel', 'React', 'Tailwind'])->map(function ($name) {
+            return Category::create([
+                'name' => $name,
+                'slug' => str($name)->slug(),
+            ]);
+        });
+        Post::factory(10)->create([
+            'user_id' => $user->id,
+        ]);
+    }
+}
